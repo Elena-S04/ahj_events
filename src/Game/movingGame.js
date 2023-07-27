dc4b3b86ae942c1227308a666145ca9e642939ad
Elena Spirina <espirina@list.ru>
2023-07-27 09:51:14 +0400

    build lint

--- src/Game/movingGame.js
+++ src/Game/movingGame.js
@@ -1,14 +1,14 @@
 export default class MovingGame {
-    moving() {
-      setInterval(() => {
-        this.goblin = document.querySelector('.item_has-goblin');
-        const randomIndex = Math.floor(1 + Math.random() * 16 - 1 + 1);
-  
-        const nextGoblin = document.getElementById(`item${randomIndex}`);
-        if (this.goblin) {
-          this.goblin.classList.remove('item_has-goblin');
-        }
-        nextGoblin.classList.add('item_has-goblin');
-      }, 1000);
-    }
+  moving() {
+    setInterval(() => {
+      this.goblin = document.querySelector('.item_has-goblin');
+      const randomIndex = Math.floor(1 + Math.random() * 16 - 1 + 1);
+
+      const nextGoblin = document.getElementById(`item${randomIndex}`);
+      if (this.goblin) {
+        this.goblin.classList.remove('item_has-goblin');
+      }
+      nextGoblin.classList.add('item_has-goblin');
+    }, 1000);
   }
+}
